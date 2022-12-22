3163c45637938bafed027996abc553411eb5f113
Sito8943 <sito8943@gmail.com>
2022-12-22 11:08:15 -0500

    init

---
+++ src/firebase/db.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBamZXL9twjayxS3bILATxRDX3O7lxkyqQ",
+  authDomain: "sito-blogger.firebaseapp.com",
+  projectId: "sito-blogger",
+  storageBucket: "sito-blogger.appspot.com",
+  messagingSenderId: "10594656839",
+  appId: "1:10594656839:web:7c446dbcabaff01443c762",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
