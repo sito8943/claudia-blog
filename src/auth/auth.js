8139fd0e0db0b843a5b520eb4b6c0365ee7c2a55
Sito8943 <sito8943@gmail.com>
2023-01-12 15:32:23 -0500

    next js init

--- src/auth/auth.js
+++
@@ -1,5 +0,0 @@
-export const getAuth = {
-  Accept: "application/json",
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "*",
-};
--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++
@@ -1,8 +0,0 @@
-// components
-import Error from "../Error/Error";
-
-const ErrorFallback = ({ error, resetErrorBoundary }) => (
-  <Error onAction={resetErrorBoundary} text={error.message} />
-);
-
-export default ErrorFallback;
